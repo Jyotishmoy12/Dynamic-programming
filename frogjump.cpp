7a35cf6c9ebfce924df3e4ce639d8b7368860a18
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-01-07 19:04:49 +0530

    Update frogjump.cpp

--- frogjump.cpp
+++ frogjump.cpp
@@ -33,5 +33,27 @@ int frogJump(int n, vector<int> &heights)
         
         dp[i]=min(fs,ss);
     }
+    
+    
     return dp[n-1];
+}
+
+//space optimization
+
+#include <bits/stdc++.h>
+int frogJump(int n, vector<int> &heights)
+{
+    int prev=0;
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int fs=prev+abs(heights[i]-heights[i-1]);
+        int ss=INT_MAX;
+        if(i>1)
+             ss=prev2+abs(heights[i]-heights[i-2]);
+        
+       int curri=min(fs,ss);
+        prev2=prev;
+        prev=curri;
+    }
+    return prev;
 }
