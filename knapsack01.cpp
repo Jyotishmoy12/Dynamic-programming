50edf668566c8ece20f1015af036d03e138c4390
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-01-20 19:13:22 +0530

    Update knapsack01.cpp

--- knapsack01.cpp
+++ knapsack01.cpp
@@ -20,3 +20,36 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
     vector<vector<int>>dp(n, vector<int>(maxWeight+1, -1));
     return f(n-1, maxWeight, weight, value, dp);
 }
+
+//TABULATION
+
+#include <bits/stdc++.h> 
+// int f(int ind, int W,vector<int>&wt, vector<int>&val,vector<vector<int>>&dp){
+//     if(ind==0){
+//         if(wt[0]<=W) return val[0];
+//         else return 0;
+//     }
+//     if(dp[ind][W]!=-1) return dp[ind][W];
+//     int notsteal=0+f(ind-1, W, wt, val,dp);
+//     int steal=INT_MIN;
+//     if(wt[ind]<=W)
+//         steal=val[ind]+f(ind-1, W-wt[ind], wt, val,dp);
+//     return dp[ind][W]= max(steal, notsteal);
+// }
+
+int knapsack(vector<int> wt, vector<int> val, int n, int maxWeight) 
+{
+	// Write your code here
+    vector<vector<int>>dp(n, vector<int>(maxWeight+1, 0));
+    for(int W=wt[0]; W<=maxWeight;W++) dp[0][W]=val[0];
+    for(int ind=1;ind<n;ind++){
+        for(int W=0;W<=maxWeight; W++){
+        int notsteal=0+dp[ind-1][W];
+    int steal=INT_MIN;
+    if(wt[ind]<=W)
+        steal=val[ind]+dp[ind-1][W-wt[ind]];
+     dp[ind][W]= max(steal, notsteal);   
+        }
+    }
+    return dp[n-1][maxWeight]; 
+}
