01a9b87af84db7b0a27bdaf019df84f0c1653c6f
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-02-14 23:49:33 +0530

    Update matrixchainmultiplcation.cpp

--- matrixchainmultiplcation.cpp
+++ matrixchainmultiplcation.cpp
@@ -16,3 +16,29 @@ int matrixMultiplication(vector<int> &arr, int N)
     vector<vector<int>>dp(N,vector<int>(N,-1));
     return f(1, N-1, arr,dp);
 }
+
+
+
+class Solution{
+public:
+    int matrixMultiplication(int N, int arr[])
+    {
+        int dp[N][N];
+        for(int i=1;i<N;i++) dp[i][i]=0;
+        
+        int mini=1e9;
+        for(int i=N-1;i>=1;i--){
+            for(int j=i+1;j<N;j++){
+                int mini=1e9;
+                for(int k=i;k<j;k++){
+                   int steps=arr[i-1]*arr[j]*arr[k]+dp[i][k]+dp[k+1][j];
+                   if(steps<mini) mini=steps;
+                }
+                 dp[i][j]=mini;
+            }
+           
+        }
+        
+        return dp[1][N-1];
+    }
+};
