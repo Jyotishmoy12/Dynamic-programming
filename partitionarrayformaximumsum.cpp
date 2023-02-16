e9a0fb52e00b02b63e4df89ff04583c7951cc97f
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-02-16 23:31:55 +0530

    Update partitionarrayformaximumsum.cpp

--- partitionarrayformaximumsum.cpp
+++ partitionarrayformaximumsum.cpp
@@ -21,3 +21,28 @@ public:
      return f(0, arr, k, dp);   
     }
 };
+
+
+class Solution {
+  
+public:
+    int maxSumAfterPartitioning(vector<int>& nums, int k) {
+     int n=nums.size();
+     vector<int>dp(n+1, 0);
+     dp[n]=0;
+     for(int ind=n-1; ind>=0;ind--){
+        int len=0;
+        int maxi=INT_MIN;
+        int maxAns=INT_MIN;
+        for(int j=ind;j<min(ind+k,n);j++){
+            len++;
+            maxi=max(maxi, nums[j]);
+            int sum=len*maxi+dp[j+1]; 
+            maxAns= max(maxAns, sum);
+        }
+       dp[ind] = maxAns; 
+     }
+     
+     return dp[0];   
+    }
+};
